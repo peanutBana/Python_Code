464af203b619bb80a2cfab210d048929a416347a
peanutBana <diao0321@gmail.com>
2023-12-25 23:16:37 +0900

    list

--- algorithm/chap01/list.py
+++ algorithm/chap01/list.py
@@ -4,5 +4,10 @@ lst03 = ['A','B','C']
 lst04 = list()
 lst05 = list('ABC')
 lst06 = list([1,2,3])
-
+lst07 = list((1,2,3))
+lst08 = list({1,2,3})
+lst09 = list(range(7))
+lst10 = list(range(3,8))
+lst11 = list(range(3,13,2))
+lst12 = [None] * 5
 
